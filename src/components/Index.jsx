95c8bceff38add9e7c97b630d9cbb1746eddf083
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-23 23:49:46 -0300

    add index.jsx main structure

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,7 +1,17 @@
-import "../assets/styles/index.css";
-
+import "../Assets/Styles/index.css"
 const Index = () => {
-  return <div>test</div>;
+  return (
+    <>
+      <div className="container">
+        <div className="name">Edelmiro Antón</div>
+        <div className="links">
+          <div className="about">About</div>
+          <div className="projects">Projects</div>
+          <div className="contact">Contact</div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Index;
