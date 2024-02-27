58610aeb6ef62233e5c876c699b7cb9827a53418
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-27 20:13:08 -0300

    set initial titles

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,13 +1,15 @@
-import "../Assets/Styles/index.css"
+import "../Assets/Styles/index.css";
 const Index = () => {
   return (
     <>
       <div className="container">
-        <div className="name">Edelmiro Antón</div>
-        <div className="links">
-          <div className="about">About</div>
-          <div className="projects">Projects</div>
-          <div className="contact">Contact</div>
+        <div className="subcontainer">
+          <div className="name">Edelmiro Antón</div>
+          <div className="links">
+            <div className="about">About</div>
+            <div className="projects">Projects</div>
+            <div className="contact">Contact</div>
+          </div>
         </div>
       </div>
     </>
