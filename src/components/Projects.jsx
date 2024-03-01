942740e779ade0bae95347e23bd3c668c4161c0e
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-02-29 23:23:43 -0300

    add loader to Projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+import Loader from "./Loader";
+
 const Projects = () => {
-  return <div>THIS IS PROJECTS COMPONENT</div>;
+  const [load, setLoad] = useState(true);
+
+  setTimeout(() => {
+    setLoad(false);
+  }, 1500);
+
+  return <>{load ? <Loader /> : <div>THIS IS PROJECTS COMPONENT</div>}</>;
 };
 
 export default Projects;
