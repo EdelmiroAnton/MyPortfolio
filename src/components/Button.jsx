f3bd769d6a60bea343b61fe2c1cc3b6811ee4623
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-01 16:15:42 -0300

    add btn styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,16 @@
 /* eslint-disable react/prop-types */
-import "../assets/styles/button.css"
+import "../assets/styles/button.css";
 
-const Button = ({children}) => {
-    return (
-        <button className='btn'>
-            {children}
-        </button>
-    );
+const Button = ({ children }) => {
+  return (
+    <button className="btn">
+      {children}
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
+  );
 };
 
 export default Button;
