c91fff2ec17e31d7f8c0be613c23bdac3a5776d6
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-09 00:04:06 -0300

    create helper with array of projects

---
+++ src/helpers/arrayOfProjects.js
@@ -0,0 +1,8 @@
+export const arrayOfProjects = [
+  {
+    id: 1,
+    project_name: "semaforo",
+    project_link: "https://edelmiroanton.github.io/trafficLight/",
+    project_code: "https://github.com/EdelmiroAnton/trafficLight",
+  },
+];
