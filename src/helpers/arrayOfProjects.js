80d3044ad54e69be34df9c2006ff1c89e73a249c
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-09 00:26:58 -0300

    add projects for testing

--- src/helpers/arrayOfProjects.js
+++ src/helpers/arrayOfProjects.js
@@ -1,7 +1,25 @@
 export const arrayOfProjects = [
   {
     id: 1,
-    project_name: "semaforo",
+    project_name: "semaforo_1",
+    project_link: "https://edelmiroanton.github.io/trafficLight/",
+    project_code: "https://github.com/EdelmiroAnton/trafficLight",
+  },
+  {
+    id: 2,
+    project_name: "semaforo_2",
+    project_link: "https://edelmiroanton.github.io/trafficLight/",
+    project_code: "https://github.com/EdelmiroAnton/trafficLight",
+  },
+  {
+    id: 3,
+    project_name: "semaforo_3",
+    project_link: "https://edelmiroanton.github.io/trafficLight/",
+    project_code: "https://github.com/EdelmiroAnton/trafficLight",
+  },
+  {
+    id: 4,
+    project_name: "semaforo_3",
     project_link: "https://edelmiroanton.github.io/trafficLight/",
     project_code: "https://github.com/EdelmiroAnton/trafficLight",
   },
