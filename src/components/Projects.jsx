0fe276978f2191d542bb1963f0fdb65c0e5fddd5
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-08 23:35:00 -0300

    add animation to title

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Loader from "./Loader";
-import "../assets/styles/projects.css"
+import "../assets/styles/projects.css";
 
 const Projects = () => {
   const [load, setLoad] = useState(true);
@@ -9,7 +9,22 @@ const Projects = () => {
     setLoad(false);
   }, 1500);
 
-  return <>{load ? <Loader /> : <div>THIS IS PROJECTS COMPONENT</div>}</>;
+  return (
+    <>
+      {load ? (
+        <Loader />
+      ) : (
+        <div className="projects_container">
+          <div className="projects_title">PROJECTS</div>
+          {/* <div>1</div>
+          <div>2</div>
+          <div>4</div>
+          <div>5</div>
+          <div>6</div> */}
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Projects;
