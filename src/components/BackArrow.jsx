69b9dbc2f28e6871263b91a58bf5c7a5d77df714
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-10 19:08:22 -0300

    add goBack functionality

--- src/components/BackArrow.jsx
+++ src/components/BackArrow.jsx
@@ -4,10 +4,22 @@ import { faCircleLeft } from "@fortawesome/free-solid-svg-icons";
 
 //Styles
 import "../assets/styles/backArrow.css";
+
+import { useNavigate } from "react-router-dom";
 const BackArrow = () => {
+  const navigate = useNavigate();
+
+  function goBack() {
+    navigate(-1);
+  }
+
   return (
     <div>
-      <FontAwesomeIcon icon={faCircleLeft} className="backArrow" />
+      <FontAwesomeIcon
+        icon={faCircleLeft}
+        className="backArrow"
+        onClick={goBack}
+      />
     </div>
   );
 };
