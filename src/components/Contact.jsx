98cc7d9f2d78df6c55b1476f9c76a2177e605891
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-03-17 18:17:14 -0300

    add basic form with Formik

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,8 +5,14 @@ import Loader from "./Loader";
 import PageTitle from "./PageTitle";
 import BackArrow from "./BackArrow";
 
+//Formik
+import { ErrorMessage, Field, Form, Formik } from "formik";
+
+//Yup
+import * as Yup from "yup";
+
 //styles
-import "../assets/styles/contact.css"
+import "../assets/styles/contact.css";
 const Contact = () => {
   const [load, setLoad] = useState(true);
 
@@ -22,6 +28,65 @@ const Contact = () => {
         <div className="contact_container">
           <BackArrow />
           <PageTitle>Contact Me!</PageTitle>
+          <Formik
+            initialValues={{ firstName: "", lastName: "", email: "" }}
+            validationSchema={Yup.object({
+              firstName: Yup.string()
+                .max(15, "Must be 15 characters or less")
+                .required("Required"),
+              lastName: Yup.string()
+                .max(20, "Must be 20 characters or less")
+                .required("Required"),
+              email: Yup.string()
+                .email("Invalid email address")
+                .required("Required"),
+            })}
+            onSubmit={(values, { setSubmitting }) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                setSubmitting(false);
+              }, 400);
+            }}
+          >
+            {(formik) => (
+              <Form action="" onSubmit={formik.handleSubmit}>
+                <label htmlFor="firstName">First Name</label>
+                <Field
+                  id="firstName"
+                  name="firstName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.firstName}
+                />
+                <ErrorMessage name="firstName" />
+
+                <label htmlFor="lastName">Last Name</label>
+                <Field
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.lastName}
+                />
+                <ErrorMessage name="lastName" />
+
+                <label htmlFor="email">Email Address</label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  onChange={formik.handleChange}
+                  onBlur={formik.handleBlur}
+                  value={formik.values.email}
+                />
+                <ErrorMessage name="email" />
+
+                <button type="submit">Submit</button>
+              </Form>
+            )}
+          </Formik>
         </div>
       )}
     </>
