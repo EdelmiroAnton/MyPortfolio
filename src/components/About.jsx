6b42263515ccf7cfe85450ccb8365cdd3a627e21
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-05-11 12:49:41 -0300

    add main structure to About Me component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import Loader from "./Loader";
 
+//Components
+import PageTitle from "./PageTitle";
+import BackArrow from "./BackArrow";
 const About = () => {
   const [load, setLoad] = useState(true);
 
@@ -8,7 +11,21 @@ const About = () => {
     setLoad(false);
   }, 1500);
 
-  return <>{load ? <Loader /> : <div>THIS IS ABOUT COMPONENT</div>}</>;
+  return (
+    <>
+      {load ? (
+        <Loader />
+      ) : (
+        <div className="aboutme_container">
+          <div className="title_container">
+            <BackArrow />
+            <PageTitle>ABOUT ME</PageTitle>
+          </div>
+          <p>PERSONAL DESCRIPTION</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default About;
