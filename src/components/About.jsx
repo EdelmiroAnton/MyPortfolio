e747b5ef5fe2bcb7f96e4ae9e59995cac1c84238
Edelmiro Anton <anton.edelmiro@gmail.com>
2024-08-30 23:51:56 -0300

    add text of personal information

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ import PageTitle from "./PageTitle";
 import BackArrow from "./BackArrow";
 
 //Styles
-import "../assets/styles/aboutMe.css"
+import "../assets/styles/aboutMe.css";
 
 const About = () => {
   const [load, setLoad] = useState(true);
@@ -25,7 +25,29 @@ const About = () => {
             <BackArrow />
             <PageTitle>ABOUT ME</PageTitle>
           </div>
-          <p>PERSONAL DESCRIPTION</p>
+          <div className="aboutme_text">
+            <p>
+              Hi! I’m Edelmiro Antón, an Argentinian Frontend developer based in
+              Valencia, Spain 🏖️.
+            </p>
+            <p>
+              My interest in the web development started in 2020 when we were in
+              quarantine due to the Covic-19 pandemic 😷. I started studying the
+              basis (HTML and CSS) and making small projects to practice 💻.{" "}
+            </p>
+            <p>
+              After that, I learned JS and ReactJS! With this two technologies I
+              could create more complex projects and started to learn the
+              programming fundamentals 🤓.
+            </p>
+            <p>
+              {" "}
+              I realize that as a developer I must be constantly watching the
+              last web development technologies applied in the market. Related
+              with this, I’m studying Typescript and also MySQL to connect apps
+              with the backend.
+            </p>
+          </div>
         </div>
       )}
     </>
